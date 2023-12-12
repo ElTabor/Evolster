b2c86f0fa48a0050f2605e289e307c75a7d9ca98
tomastaboada27 <tomastaboada27@gmail.com>
2023-12-12 03:41:47 -0300

    HeavyEnemy fix (try)

--- Evolster/Assets/Scripts/Enemies/HeavyEnemy.cs
+++ Evolster/Assets/Scripts/Enemies/HeavyEnemy.cs
@@ -13,41 +13,19 @@ public class HeavyEnemy : Enemy
     {
         base.Update();
         if (!lifeController.dead && !_charging) navMesh.SetDestination(player.position);
-        if (player.transform.position.x - transform.position.x >= 0) transform.localScale = new Vector3(-1, 1, 1);
-        else transform.localScale = new Vector3(1, 1, 1);
-
-        distance = Vector2.Distance(player.transform.position, transform.position);
-        isInRangeAttack = distance <= enemyStats.attackRange;
-
-        if(!lifeController.dead)
-        {
-            if (!_charging) navMesh.SetDestination(player.position);
-
-            if (isInRangeAttack && Time.time > lastAttack + attackCooldown)
-            {
-                animator.SetBool("Attacking", true);
-                Attack();
-            }
-        }
     }
 
-    protected override void Attack()
-    {
-        StartCoroutine(Charge());
-    }
+    protected override void Attack() => StartCoroutine(Charge());
 
     private IEnumerator Charge()
     {
-        //animator.SetBool("Charging", false);
         animator.SetBool("Attacking", true);
         _charging = true;
         navMesh.speed = chargingSpeed;
-        navMesh.acceleration = 20;
         navMesh.SetDestination(player.transform.position);
         yield return new WaitForSeconds(chargingTime);
         animator.SetBool("Attacking", false);
         _charging = false;
-        navMesh.acceleration = 8;
         navMesh.speed = enemyStats.movementSpeed;
     }
 
