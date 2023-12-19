9012b14ba30950e5e6ac76214a858437a1260f98
tomastaboada27 <tomastaboada27@gmail.com>
2023-12-19 03:04:49 -0300

    Final commit

--- Evolster/Assets/Scripts/Enemies/Enemy.cs
+++ Evolster/Assets/Scripts/Enemies/Enemy.cs
@@ -187,7 +187,7 @@ public class Enemy : MonoBehaviour
                 break;
         }
         source.clip = clipToPlay;
-        source.PlayOneShot(clipToPlay);
+        source.PlayOneShot(clipToPlay, AudioController.instance.sfxVolume);
     }
     private void OnDrawGizmos()
     {
